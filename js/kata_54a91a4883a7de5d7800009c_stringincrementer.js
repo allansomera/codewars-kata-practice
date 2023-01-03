a0aa32536abd1d349664e2cca83f509b43abee35
Allan Somera <nagal.somera@gmail.com>
2023-01-03 03:12:05 -0800

    solved string incrementer challenge

--- js/kata_54a91a4883a7de5d7800009c_stringincrementer.js
+++ js/kata_54a91a4883a7de5d7800009c_stringincrementer.js
@@ -1,4 +1,5 @@
 const check09inc = (s) => {
+  if (s === undefined) return 1
   let re_0 = /^(.*0{1,})/
   let re_9 = /(9{1,})$/
   let num = 0
@@ -10,32 +11,42 @@ const check09inc = (s) => {
   // if zeroes exist in string
   if (re_0.test(s)) {
     let m0 = s.match(re_0)
-    // console.log(m0)
+    console.log("zeroes exist: ", m0)
     let z_len = m0[1].length
     let m9 = s.match(re_9)
-    if(re_9.test(s)){
+    if (re_9.test(s)) {
+      console.log("9s exist")
       let m9 = s.match(re_9)
       //check if the 9s is less than the length of the string including the zeroes
       num = +m9[1]
       ++num
       let num_len = num.toString().length
       //when inc, and a new digit position is added
-      if(num_len > m9[1].length){
-        let new_0_str = s.slice(0,m0[1].length-1)
+      if (num_len > m9[1].length) {
+        let new_0_str = s.slice(0, m0[1].length - 1)
         return new_0_str + num
       }
-    }
-    else{
-      let new_0_str = s.slice(0,m0[1].length)
-      console.log("new_0_str", new_0_str)
-      num = s.charAt(s.length - 1)
-      console.log(num)
-      return new_0_str + (++num)
+    } else {
+      //check if right most digit is a zero
+      if (s.charAt(s.length - 1) === "0") {
+        let new_0_str = s.slice(0, m0[1].length - 1)
+        num = +s.charAt(s.length - 1)
+        console.log("right most zero", new_0_str)
+        console.log("added increment", new_0_str + (num + 1))
+        return new_0_str + ++num
+      } else {
+        // let new_0_str = s.slice(0, m0[1].length)
+        // console.log("new_0_str", new_0_str)
+        console.log("there are no 9s: ", m0[1])
+        num = +s.charAt(s.length - 1)
+        console.log("right most digit not zero", m0[1] + (num + 1))
+        // console.log(num)
+        return m0[1] + ++num
+      }
       // console.log("final", new_0_str + (++num))
-
+      console.log("inside check09inc")
     }
-  } 
-  else {
+  } else {
     num = +s
     return ++num
   }
@@ -43,13 +54,26 @@ const check09inc = (s) => {
 function incrementString(strng) {
   const re = /^(\w+?(?:\d.*?)?\w+?)?([0-9]+)?$/
   const res = strng.match(re)
-  const [whole, first, num, ...rest] = res
+  let [whole, first, num, ...rest] = res
   // console.log("inc", check09inc(num))
-  // console.log(res)
-  // console.log(whole)
-  // console.log(first)
-  // console.log(num)
+  console.log(res)
+  console.log(whole)
+  console.log(first)
+  console.log(num)
   // return first + check09inc(num)
+  if (first === undefined && num) {
+    let n = ++num
+    console.log("n is", n)
+  } else if (first === undefined) {
+    console.log("first undefined")
+    console.log("" + check09inc(num))
+  } else if (first && num === undefined) {
+    if (first.charAt(0) !== "0") {
+      return first + 1
+    } else {
+      console.log(check09inc(first))
+    }
+  }
   console.log(first + check09inc(num))
 }
-incrementString("foobar000")
+incrementString("009")
