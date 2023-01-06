bc8588e6ec5d5b17304a17625c76ee317d8c1abe
Allan Somera <nagal.somera@gmail.com>
2023-01-05 23:40:48 -0800

    added conditions to break when str are only spaces

--- js/kata_52449b062fb80683ec000024_thehashtaggenerator.js
+++ js/kata_52449b062fb80683ec000024_thehashtaggenerator.js
@@ -1,4 +1,7 @@
 function generateHashtag(str) {
+  if(str === "") return false
+  let rep_space = str.replace(/ +/g, ' ')
+  if(rep_space === " ") return false
   let arr = str.split(" ")
   let new_arr = arr.map(
     (i) => i.charAt(0).toUpperCase().replace(/^/, "#") + i.slice(1)
@@ -7,3 +10,4 @@ function generateHashtag(str) {
 }
 
 generateHashtag("Do We have A Hashtag")
+generateHashtag(" ".repeat(20))
