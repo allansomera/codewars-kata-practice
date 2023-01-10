bec0bb4ad017fb82d96e15768255d19ee668e0d4
Allan Somera <nagal.somera@gmail.com>
2023-01-09 19:19:19 -0800

    trying to solve after not working on it for a few days

--- js/kata_52bc74d4ac05d0945d00054e_firstnonrepeatingchar.js
+++ js/kata_52bc74d4ac05d0945d00054e_firstnonrepeatingchar.js
@@ -1,19 +1,22 @@
 function firstNonRepeatingLetter(s) {
-    if (s === "") return 
-    let k = {}
-    s.split("").forEach(i => k[i] = ++k[i] || 1)
-    if(Object.keys(k).length === Object.entries(k).filter(( [key, value] ) => value >=2 ).length){
-        // console.log("string has repeats on each character")
-        return ""
-    }
-    
-    // console.log(k)
-    console.log(Object.entries(k).filter(([key, val]) => val === 1 )[0][0])
-    // console.log(Object.fromEntries(Object.entries(k).filter(([key, val]) => val === 1 )))
-    // return Object.entries(k).filter(([key, val]) => val === 1 )[0][0]
+  if (s === "") return
+  let k = {}
+  s.split("").forEach((i) => (k[i] = ++k[i] || 1))
+  if (
+    Object.keys(k).length ===
+    Object.entries(k).filter(([key, value]) => value >= 2).length
+  ) {
+    // console.log("string has repeats on each character")
+    return ""
+  }
+
+  Object.entries(k).forEach((key, value) => console.log(value))
+  console.log(Object.entries(k))
+  console.log(Object.entries(k).filter(([key, val]) => val === 1)[0][0])
+  // console.log(Object.fromEntries(Object.entries(k).filter(([key, val]) => val === 1 )))
+  // return Object.entries(k).filter(([key, val]) => val === 1 )[0][0]
 }
 
-
 // firstNonRepeatingLetter('ssttsrrsaabbcc')
-firstNonRepeatingLetter('sTreSS')
+firstNonRepeatingLetter("sTreSS")
 // firstNonRepeatingLetter('a')
