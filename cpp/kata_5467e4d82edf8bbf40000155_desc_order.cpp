4f1d1e6b04dd8ed3e62b5d98c49b65b060f3b21b
Allan Somera <nagal.somera@gmail.com>
2023-01-21 03:25:06 -0800

    finished kata 5467e4d82edf8bbf40000155 cpp

--- cpp/kata_5467e4d82edf8bbf40000155_desc_order.cpp
+++ cpp/kata_5467e4d82edf8bbf40000155_desc_order.cpp
@@ -1,14 +1,32 @@
 #include <cinttypes>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
-uint64_t descendingOrder(uint64_t);
 
 uint64_t descendingOrder(uint64_t a)
 {
     std::string str = std::to_string(a);
-    std::cout << "a to str: " << str << std::endl;
-    return 0;
+    std::vector<int> v;
+    std::string res;
+    // std::cout << "a to str: " << str << std::endl;
+    for (const auto& ch: str){
+        int num = std::stoi(std::string(1,ch));
+        v.push_back(num);
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    std::sort(v.begin(), v.end(), std::greater<>());
+    
+    for(int i: v) {
+        std::cout << i << " ";
+        res += std::to_string(i);
+    }
+    //using std::stoul because stoi reached max limit for int
+    return (uint64_t)std::stoul(res);
+    
 }
 
 int main(){
