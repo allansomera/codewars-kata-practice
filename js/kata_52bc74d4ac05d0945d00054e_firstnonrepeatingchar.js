8ab4772139ffac4d5d1e2d92a6f3bdb19b8697f3
Allan Somera <nagal.somera@gmail.com>
2023-01-22 23:48:51 -0800

    solved First non-repeating character kata

--- js/kata_52bc74d4ac05d0945d00054e_firstnonrepeatingchar.js
+++ js/kata_52bc74d4ac05d0945d00054e_firstnonrepeatingchar.js
@@ -1,7 +1,12 @@
 function firstNonRepeatingLetter(s) {
   if (s === "") return
   let k = {}
-  s.split("").forEach((i) => (k[i] = ++k[i] || 1))
+  s.split("").forEach((i) => {
+    // if (i == i.toLowerCase()) k[i.toLowerCase()] = ++k[i.toLowerCase()] || 1
+    // else if (i == i.toUpperCase())
+    //   k[i.toLowerCase()] = ++k[i.toLowerCase()] || 1
+    k[i.toLowerCase()] = ++k[i.toLowerCase()] || 1
+  })
   if (
     Object.keys(k).length ===
     Object.entries(k).filter(([key, value]) => value >= 2).length
@@ -10,11 +15,30 @@ function firstNonRepeatingLetter(s) {
     return ""
   }
 
-  Object.entries(k).forEach((key, value) => console.log(value))
-  console.log(Object.entries(k))
-  console.log(Object.entries(k).filter(([key, val]) => val === 1)[0][0])
+  // Object.entries(k).forEach((key, value) => console.log(value))
+
+  // console.log(Object.entries(k))
+  // re = new RegExp(
+  //   Object.entries(k).filter(([key, val]) => val === 1)[0][0],
+  //   "gi"
+  // )
+
+  // console.log(Object.entries(k).filter(([key, val]) => val === 1)[0][0])
+  console.log(
+    s.match(
+      new RegExp(
+        Object.entries(k).filter(([key, val]) => val === 1)[0][0],
+        "gi"
+      )
+    )[0]
+  )
+  // console.log(m)
+
   // console.log(Object.fromEntries(Object.entries(k).filter(([key, val]) => val === 1 )))
   // return Object.entries(k).filter(([key, val]) => val === 1 )[0][0]
+  return s.match(
+    new RegExp(Object.entries(k).filter(([key, val]) => val === 1)[0][0], "gi")
+  )[0]
 }
 
 // firstNonRepeatingLetter('ssttsrrsaabbcc')
