85572ac082533c0259e5f17fbe95f1f76583113b
Allan Somera <nagal.somera@gmail.com>
2023-02-04 08:11:51 -0800

    solved kata 55c6126177c9441a570000cc

--- python/kata_56a5d994ac971f1ac500003e_consectutive_strings.py
+++ python/kata_56a5d994ac971f1ac500003e_consectutive_strings.py
@@ -1,30 +1,47 @@
-from itertools import tee, islice, chain, repeat
+from itertools import tee, islice, chain, repeat, pairwise
+from more_itertools import sliding_window
 
 
-def pairwise(itt):
-    a, b = tee(itt)
-    next(b, None)
-    return zip(a, b)
+# def pairwise(itt):
+#     a, b = tee(itt)
+#     next(b, None)
+#     return zip(a, b)
 
 
 def longest_consec(strarr, k):
-    for _ in range(0, len(strarr), k):
-        print(strarr[::k])
+    return (
+        ""
+        if len(strarr) == 0 or k <= 0 or k > len(strarr)
+        else sorted(
+            ["".join(x) for x in sliding_window(strarr, k)], key=len, reverse=True
+        )[0]
+    )
 
 
-def chunk_pad(it, size, padval=None):
-    it = chain(iter(it), repeat(padval))
-    return iter(lambda: tuple(islice(it, size)), (padval,) * size)
+# def chunk_pad(it, size, padval=""):
+#     it = chain(iter(it), repeat(padval))
+#     return iter(lambda: tuple(islice(it, size)), (padval,) * size)
 
 
 def main():
     l = ["zone", "abigail", "theta", "form", "libe", "zas"]
-    ll = list(chunk_pad(l, 3))
+    ll = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"]
+    # ll = list(chunk_pad(l, 2))
     # longest_consec(l, 2)
-    print("pairwise\n")
-    print(ll)
+    # lll = pairwise(l)
+    # print("pairwise\n")
+    # print(lll)
+    # for x in ll:
+    #     print(x)
     # for x, y in pairwise(l):
     #     print(x, y)
+
+    # print(sorted(res, key=len, reverse=True))
+    # print(res)
+    # res = sliding_window(l, 2)
+    # res = ["".join(x) for x in sliding_window(ll, 2)]
+    # print(sorted(res, key=len, reverse=True)[0])
+    print(longest_consec(l, 2))
 
 
 if __name__ == "__main__":
