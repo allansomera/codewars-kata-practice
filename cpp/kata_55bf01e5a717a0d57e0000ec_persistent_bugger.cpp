20d8be7bcb8687ba2cff7f08104a86b1eb012407
Allan Somera <nagal.somera@gmail.com>
2023-02-16 00:02:44 -0800

    finished kata 55bf01e5a717a0d57e0000ec persistent bugger cpp

--- cpp/kata_55bf01e5a717a0d57e0000ec_persistent_bugger.cpp
+++ cpp/kata_55bf01e5a717a0d57e0000ec_persistent_bugger.cpp
@@ -13,11 +13,13 @@ using std::end;
 
 int persistence(long long n){
     vector<int> v;
+    cout << endl;
     // int count = 0;
     // for(;n>0;n/=10)
     //     count++;
     
     //single out a digit from n and store in vector
+    cout << "n is: " << n << endl;
     for(;n>0;) {
         v.push_back(n%10);
         n/=10;
@@ -26,16 +28,17 @@ int persistence(long long n){
     
     //multiply each element with each other
     int count = 0;
-    for(;v.size() != 1; count++){
+    for(;v.size() != 1; ++count){
         auto multi = std::accumulate(begin(v), end(v), 1, std::multiplies<>());
         cout << "multi is: " << multi << endl;
-
         v.clear();
         for(;multi>0;) {
             v.push_back(multi%10);
             multi/=10;
         }
+        if(v.size() == 0 || v.size() == 1 ) return ++count;
         
+        cout << "v.size() is: " << v.size() << endl;
     }
     // cout << multi;
     //print in reverse
@@ -45,6 +48,10 @@ int persistence(long long n){
 
 
 int main(){
-    cout << persistence(25);
+    cout << persistence(39) << endl;
+    cout << persistence(4) << endl;
+    cout << persistence(25) << endl;
+    cout << persistence(999) << endl;
+    cout << persistence(444) << endl;
     return 0;
 }
