ad90f7fe8c9e0f6558671e49dcc71f3823e21adf
Allan Somera <nagal.somera@gmail.com>
2023-02-21 23:48:51 -0800

    got function working on most terms, need to test edge cases and add if statement for x^1

--- python/kata_56d060d90f9408fb3b000b03_derivative_of_polynomial.py
+++ python/kata_56d060d90f9408fb3b000b03_derivative_of_polynomial.py
@@ -2,48 +2,89 @@ der = []
 
 
 def derivative(eq):
+    global der
     terms = eq.split("+")
     for i in terms:
         x = check_term(i)
+    der_string = f"{'+'.join(der)}"
+    return der_string
 
 
 def check_term(t):
-    # check for exponent
-    coeff = 0
-    exp = 0
-    exp_d = 0
-    coeff_res = 0
-    term_res = ""
-    if "^" in t:
-        # if true, asssume x is the character before '^'
-        print(t.split("^"))
-        exp_d = int(t.split("^")[1])
-        print(f"exp_d: {exp_d}")
-        print(t.split("^")[0].split("x"))
-        coeff = int(t.split("^")[0].split("x")[0])
-        print(f"coeff: {coeff}")
-        if exp_d >= 1:
-            exp = exp_d - 1
-        else:
-            exp = 0
-        coeff_res = coeff * exp_d
-        print(f"coeff_res: {coeff_res}")
-        term_res = f"{coeff_res}x^{exp}"
-        print(f"term_res: {term_res}\n")
-    else:
-        # no exponent '^'
-        # check for x
-        if "x" in t:
-            print("has x in term")
-            chk_x = t.split("x")
-            print(chk_x)
-            if len(chk_x) == 1:
-                print("no x")
-            # no x
-
+    # check for exponent, split by 'x^'
     global der
-    der.append(term_res)
-    # print(der)
+    exp_i = 0
+    coeff_i = 0
+    coeff_res = 0
+    exp_right = 0
+    term_res = ""
+    der_string = []
+    if "^" in t:
+        t2 = t.split("x^")
+        # print(f"t2 is: {t2}")
+        # t2 is now a list
+        if "-" in t2[0]:
+            print("negative sign detected")
+            if len(t2[0]) == 1:
+                # single x with negative sign '-x'
+                print("single -x")
+                coeff_i = -1
+                exp_i = int(t2[1])
+                exp_right = int(exp_i) - 1
+                coeff_res = coeff_i * exp_i
+                print(f"negative coeff only 1: {coeff_res}")
+                term_res = f"{coeff_res}x^{exp_right}"
+                der.append(term_res)
+                print(f"{term_res}")
+            else:
+                print("negative number")
+                coeff_i = int(t2[0])
+                exp_i = int(t2[1])
+                exp_right = int(exp_i) - 1
+                coeff_res = coeff_i * exp_i
+                print(f"negative coeff: {coeff_res}")
+                term_res = f"{coeff_res}x^{exp_right}"
+                der.append(term_res)
+                print(f"{term_res}")
+        else:
+            # print(f"t2 is: {t2}")
+            coeff_i = int(t2[0])
+            exp_i = int(t2[1])
+            exp_right = int(exp_i) - 1
+            coeff_res = coeff_i * exp_i
+            print(f"{coeff_res}x^{exp_right}")
+            term_res = f"{coeff_res}x^{exp_right}"
+            der.append(term_res)
+    elif "x" in t:
+        # only 1 x
+        t2 = t.split("x")
+        if "-" in t2[0]:
+            if len(t2[0]) == 1:
+                # single x with negative sign '-x'
+                coeff_i = -1
+                exp_i = int(t2[1])
+                exp_right = int(exp_i) - 1
+                coeff_res = coeff_i * exp_i
+                print(f"negative coeff only 1: {coeff_res}")
+            else:
+                coeff_i = int(t2[0])
+                exp_i = int(t2[1])
+                exp_right = int(exp_i) - 1
+                coeff_res = coeff_i * exp_i
+                print(f"negative coeff: {coeff_res}")
+        else:
+            coeff_i = int(t2[0])
+            exp_i = 1
+            coeff_res = coeff_i * exp_i
+            term_res = f"{coeff_res}"
+            der.append(term_res)
+            print(f"{coeff_res}\n")
+    else:
+        coeff_res = 0
+
+    # global der
+    # der_string = "+".join(der)
+    # return der_string
 
     # print(coeff)
     # print(exp)
@@ -52,16 +93,18 @@ def check_term(t):
     # if true, multiply by 1, return product
     # else return 1
     # print(t.split("^"))
-    pass
 
 
 def main():
     # derivative("4x^2+2x+1")
-    print("5x^3+2x^2+4")
-    derivative("5x^3+2x^2+4")
+    print("5x^3+2x^2+2x+4")
+    print(derivative("-5x^3+2x^2+2x+4"))
+    # print("-x^3+2x^2+2x+4")
+    # print(derivative("-x^3+2x^2+2x+4"))
+    # print(f"{'+'.join(der)}")
 
-    print("2x+1")
-    derivative("2x+1")
+    # print("2x+1")
+    # derivative("2x+1")
 
 
 if __name__ == "__main__":
