f585ff143a6eefdaf2a97ad3ef1cb133305d6f6b
Allan Somera <nagal.somera@gmail.com>
2023-03-09 22:08:52 -0800

    finished kata 57a0e5c372292dd76d000d7e string repeat rust

--- rust/kata_57a0e5c372292dd76d000d7e_string_repeat.rs
+++ rust/kata_57a0e5c372292dd76d000d7e_string_repeat.rs
@@ -1,3 +1,24 @@
 fn repeat_str(src: &str, count: usize) -> String {
-  String::new()
+  let mut z = String::new();
+  for  _ in 0..count{
+      z.push_str(src)
+  }
+  return z;
 }
+
+fn main(){
+    let src = String::from("test");
+    let num = 5;
+    println!("{}",repeat_str(&src,num));
+}
+
+// 1 index
+// for _ in 1..=count {
+//     ret.push_str(src);
+// }
+
+// soln
+// fn repeat_str(src: &str, count: usize) -> String {
+//   std::iter::repeat(src).take(count).collect()
+// }
+
