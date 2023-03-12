28bf5ba16f82b85fdc536ba451aa6b90dd9b761b
Allan Somera <nagal.somera@gmail.com>
2023-03-11 22:35:39 -0800

    taking a break on kata 52597aa56021e91c93000cb0, i need a new way to tackle this problem

--- js/kata_63fb421be6be1f57ad81809e_decode_the_woofs.js
+++ js/kata_63fb421be6be1f57ad81809e_decode_the_woofs.js
@@ -12,20 +12,42 @@ function woofDecoder(str) {
     split_arr = [...str.split("!")].filter((i) => i)
     let message = []
     if (split_arr.length > 0) {
-      if (split_arr[0].match(pattern) !== null) {
-        // console.log(split_arr)
-        split_arr.forEach((i) => {
-          // console.log(alpha_string[i.match(pattern).length - 1])
-          message.push(alpha_string[i.match(pattern).length - 1])
-        })
-        console.log(message.toString().split(",").join(""))
-      } else {
-        if (split_arr[0].match(pattern2) !== null) {
-          split_arr.forEach((i) => {
-            console.log(i.match(pattern2))
+      // console.log(split_arr)
+      let count = 0
+      split_arr.forEach((i) => {
+        if (i.includes("-")) {
+          split_dash = i.split("-")
+          let count = 0
+          split_dash.forEach((x) => {
+            if (x.match(pattern).length >= 1) {
+              console.log(x.match(pattern))
+              count++
+            }
+            // else {
+            //   count++
+            //   console.log(`x is: ${x}`)
+            // }
+            // count++
           })
+          message.push(alpha_string[count - 1])
         }
-      }
+        // if (i.match(pattern) !== null) {
+        //   let len = i.match(pattern).length
+        //   message.push(alpha_string[i.match(pattern).length - 1])
+        // } else if (i.match(pattern2) !== null) {
+        //   console.log(`pattern2: ${i.match(pattern2)}`)
+        // }
+        // console.log(alpha_string[i.match(pattern).length - 1])
+        // console.log(i.match(pattern))
+      })
+      console.log(message.toString().split(",").join(""))
+      // else {
+      //   if (split_arr[0].match(pattern2) !== null) {
+      //     split_arr.forEach((i) => {
+      //       console.log(i.match(pattern2))
+      //     })
+      //   }
+      // }
     }
     // else if (split_arr.length === 0) {
     //   console.log("use pattern2")
@@ -37,14 +59,15 @@ function woofDecoder(str) {
   // console.log(split_arr)
 }
 
-woofDecoder(
-  "aWoof-woof-woof-woof-woof-woof-woof!woof woof woof! woof woof woof woof woof!"
-)
-woofDecoder(
-  "Woof-woof-woof-woof-woof-woof!Woof-woof-woof-woof-woof! " +
-    "Woof-woof-woof-woof-woof! Woof-woof-woof-woof! " +
-    "Woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof! " +
-    "Woof-woof-woof-woof-woof!"
-)
+// woofDecoder(
+//   "aWoof-woof-woof-woof-woof-woof-woof!woof woof woof! woof woof woof woof woof!"
+// )
+woofDecoder("Woof! Woof!" + "-woof".repeat(26) + "!")
+// woofDecoder(
+//   "Woofshhhwoof-woof-woof-woof-woof-woof!Woof-woof-woof-woof-woof! " +
+//     "Woof-woof-woof-woof-woof! Woof-woof-woof-woof! " +
+//     "Woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof-woof! " +
+//     "Woof-woof-woof-woof-woof!"
+// )
 // woofDecoder("awoof")
 // woofDecoder("awereroereroerefer!")
