80a5496f3d7634e7b7156bf58b4e6829669a3990
Allan Somera <nagal.somera@gmail.com>
2023-03-17 02:15:16 -0700

    finished kata 52e88b39ffb6ac53a400022e int32 to IPv4 js

--- js/kata_52e88b39ffb6ac53a400022e_int32_to_IPv4.js
+++ js/kata_52e88b39ffb6ac53a400022e_int32_to_IPv4.js
@@ -1,5 +1,23 @@
-function int32toIp(int32) {
-  return (int32 >>> 0).toString(2)
+function int32ToIp(int32) {
+  // let x = (int32 >>> 0)
+  return (int32 >>> 0)
+    .toString(2)
+    .padStart(32, "0")
+    .match(/.{1,8}/g)
+    .map((i) => parseInt(i, 2).toString())
+    .join(".")
+  // return (int32 >>> 0).toString(2).padStart(32, "0")
+  // return x
 }
 
-console.log(int32toIp(9))
+console.log(int32toIp(2149583361))
+// console.log(int32toIp(8))
+
+// solutions:
+// const int32ToIp = int32 => [24, 16, 8, 0].map(e => int32 >> e & 255).join('.');
+//
+// const int32ToIp = int32 => [24, 16, 8, 0].map(e => int32 >> e & 255).join`.`;
+//
+// function int32ToIp(int32){
+//   return [int32 >>> 24, int32 >> 16 & 255, int32 >> 8 & 255, int32 & 255].join('.');
+// }
