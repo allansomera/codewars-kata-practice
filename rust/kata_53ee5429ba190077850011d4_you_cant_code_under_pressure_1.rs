ca19a839511e96b8fba6755de22b13a237e305f9
Allan Somera <nagal.somera@gmail.com>
2023-04-13 16:34:59 -0700

    added solutions for kata 53ee5429ba190077850011d4

--- rust/kata_53ee5429ba190077850011d4_you_cant_code_under_pressure_1.rs
+++ rust/kata_53ee5429ba190077850011d4_you_cant_code_under_pressure_1.rs
@@ -8,3 +8,26 @@ fn main() {
     let num = double_integer(1);
     println!("{}", num);
 }
+
+// solutions:
+//
+// fn double_integer(n: i32) -> i32 {
+//     n<<1
+// }
+//
+// fn double_integer(n: i32) -> i32 {
+//     n + n
+// }
+// fn double_integer(n: i32) -> i32 {
+//     let mynum: i32 = n*2;
+//     return mynum;
+// }
+//
+// fn double_integer(n: i32) -> i32 {
+//     // be fast
+//     let x: i32 = &n + &n;
+//     x
+// }
+
+// no need for a semi-colon because having no semi-colon eveluates to a value,
+// a semi-colon means it is an expression
