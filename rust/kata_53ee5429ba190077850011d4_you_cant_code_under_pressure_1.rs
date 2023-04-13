691e7fa00d06308626978a5c65fe10dd4e2c3e1f
Allan Somera <nagal.somera@gmail.com>
2023-04-13 16:30:20 -0700

    finished kata 53ee5429ba190077850011d4 rust

---
+++ rust/kata_53ee5429ba190077850011d4_you_cant_code_under_pressure_1.rs
@@ -0,0 +1,10 @@
+fn double_integer(n: i32) -> i32 {
+    return n * 2;
+}
+
+fn main() {
+    // let src = String::from("test");
+    // let num = 5;
+    let num = double_integer(1);
+    println!("{}", num);
+}
