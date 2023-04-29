1937da74f884bc7db8992023192026d2d1910509
Allan Somera <nagal.somera@gmail.com>
2023-04-28 21:47:09 -0700

    finished kata 5899dc03bc95b1bf1b0000ad, invert values, rust

---
+++ rust/kata_5899dc03bc95b1bf1b0000ad_invert_values.rs
@@ -0,0 +1,29 @@
+fn invert(values: &[i32]) -> Vec<i32> {
+    let vec: Vec<_> = values
+        .iter()
+        .enumerate()
+        .map(|(_, &el)| if el > 0 { el * -1 } else { i32::abs(el) })
+        .collect();
+    vec
+}
+
+fn main() {
+    println!("{:?}", invert(&vec![-1, 2, 3, 4, 5]))
+}
+
+// soln:
+// fn invert(values: &[i32]) -> Vec<i32> {
+//     values.iter().map(|x| -x).collect()
+// }
+//
+// fn invert(values: &[i32]) -> Vec<i32> {
+//     values.iter().map(std::ops::Neg::neg).collect()
+// }
+//
+// fn invert(values: &[i32]) -> Vec<i32> {
+//     values.iter().map(|v| {-v}).collect()
+// }
+//
+// fn invert(values: &[i32]) -> Vec<i32> {
+//      values.iter().map(|i| *i * -1).collect()
+// }
