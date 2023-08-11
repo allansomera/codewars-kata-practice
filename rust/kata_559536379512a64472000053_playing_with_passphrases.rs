b9bf38ee3de50e1df06d1f1c6261bd4e308408ae
Allan Somera <nagal.somera@gmail.com>
2023-08-10 23:10:44 -0700

    working on kata 559536379512a64472000053

--- rust/kata_559536379512a64472000053_playing_with_passphrases.rs
+++ rust/kata_559536379512a64472000053_playing_with_passphrases.rs
@@ -1,5 +1,14 @@
 fn play_pass(s: &str, n: u32) -> String {
-    let ss: Vec<String> = s.split_whitespace().map(str::to_string).collect::<Vec<_>>();
+    let ss: Vec<String> = s
+        .split_whitespace()
+        .map(str::to_string)
+        .collect::<Vec<_>>()
+        .into_iter()
+        .rev()
+        .collect::<Vec<_>>()
+        .into_iter()
+        .map(|x| x.chars().rev().collect::<String>())
+        .collect();
     println!("{:?}", ss);
     "test".to_string()
 }
