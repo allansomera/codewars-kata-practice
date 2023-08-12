fc1f439120647bbd8065bb986fa6f4bbbf64f9b5
Allan Somera <nagal.somera@gmail.com>
2023-08-11 21:39:52 -0700

    worked on 559536379512a64472000053

--- rust/kata_559536379512a64472000053_playing_with_passphrases.rs
+++ rust/kata_559536379512a64472000053_playing_with_passphrases.rs
@@ -9,8 +9,52 @@ fn play_pass(s: &str, n: u32) -> String {
         .into_iter()
         .map(|x| x.chars().rev().collect::<String>())
         .collect();
-    println!("{:?}", ss);
-    "test".to_string()
+
+    // let letter = 'b';
+    // print!("{}:{} => ", letter, 'a' as u8 + (letter as u8 - 'a' as u8));
+    // println!(
+    //     "{}:{}",
+    //     ('a' as u8 + ((letter as u8 - 'a' as u8) + 1) % 26) as char,
+    //     ('a' as u8 + ((letter as u8 - 'a' as u8) + 1) % 26)
+    // );
+    //
+    // println!("{:?}", ss);
+    ss.iter()
+        .map(|x| {
+            let mut c: char = '\0';
+            x.chars()
+                .map(|ch| {
+                    match ch {
+                        'A'..='Z' => {
+                            c = ('A' as u8 + ((ch as u8 - 'A' as u8) + n as u8) % 26) as char
+                        }
+                        'a'..='z' => {
+                            c = ('a' as u8 + ((ch as u8 - 'a' as u8) + n as u8) % 26) as char
+                        }
+                        // 'A'..='Z' => 'X',
+                        // 'a'..='z' => 'x',
+                        _ => c = ch,
+                    };
+                    c
+                })
+                .collect::<String>()
+        })
+        .collect::<Vec<_>>()
+        .join(" ")
+    //
+    // println!("{:?}", ss);
+    // for i in ss.iter() {
+    //     for x in i.chars() {
+    //         // println!("{}", x)
+    //         let c: char;
+    //         match x {
+    //             'A'..='Z' => c = 'x',
+    //             _ => c = 'b',
+    //         }
+    //         println!("{}", c)
+    //     }
+    // }
+    // "test".to_string()
 }
 
 fn main() {
