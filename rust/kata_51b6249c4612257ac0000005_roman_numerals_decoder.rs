dab48592aefe685af1ff422fa37c205813e68586
Allan Somera <nagal.somera@gmail.com>
2023-08-15 23:54:25 -0700

    worked on kata 51b6249c4612257ac0000005

--- rust/kata_51b6249c4612257ac0000005_roman_numerals_decoder.rs
+++ rust/kata_51b6249c4612257ac0000005_roman_numerals_decoder.rs
@@ -1,6 +1,8 @@
 fn roman_as_num(roman: &str) -> u64 {
     let vs: Vec<String> = roman.chars().map(|x| x.to_string()).collect::<Vec<_>>();
+    let find_x = vs.iter().position(|x| x == "X").unwrap();
     println!("{:?}", vs);
+    println!("{:?}", find_x);
     1
 }
 
