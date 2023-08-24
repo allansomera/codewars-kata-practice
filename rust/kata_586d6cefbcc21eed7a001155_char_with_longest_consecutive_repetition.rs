7624354421703857f361b8fc87b00e70c103ab08
Allan Somera <nagal.somera@gmail.com>
2023-08-23 23:30:04 -0700

    worked on kata 586d6cefbcc21eed7a001155

--- rust/kata_586d6cefbcc21eed7a001155_char_with_longest_consecutive_repetition.rs
+++ rust/kata_586d6cefbcc21eed7a001155_char_with_longest_consecutive_repetition.rs
@@ -1,5 +1,11 @@
+// extern crate regex::Regex;
+use regex::regex;
+
 fn longest_repetition(s: &str) -> Option<(char, usize)> {
-    unimplemented!();
+    let re = Regex::new(r"(.)\1*").unwrap();
+    let Some(cap) = re.captures(s) else {return};
+    println!(":?", cap);
+    Option('a', 1)
 }
 
 fn main() {
