21a7c0c708a9da4104f8188a9a702d23b3d44361
Allan Somera <nagal.somera@gmail.com>
2023-08-27 21:51:39 -0700

    worked on 5526fc09a1bbd946250002dc

--- rust/kata_5526fc09a1bbd946250002dc_find_the_parity_outlier.rs
+++ rust/kata_5526fc09a1bbd946250002dc_find_the_parity_outlier.rs
@@ -1,7 +1,25 @@
+use std::collections::HashMap;
+
 fn find_outlier(values: &[i32]) -> i32 {
-    todo!()
+    // let hs: HashMap<i32, i32> = HashMap::new();
+    let hs: HashMap<i32, i32> = values.iter().fold(HashMap::new(), |mut m, c| {
+        if c % 2 == 0 {
+            *m.entry(0).or_insert(0) += 1;
+        } else {
+            *m.entry(1).or_insert(0) += 1;
+        }
+        m
+    });
+    if *hs.get(&0).unwrap() as i32 >= 2 {
+        values[values.iter().position(|&x| x % 2 == 1).unwrap() as usize]
+    } else {
+        values[values.iter().position(|&x| x % 2 == 0).unwrap() as usize]
+    }
+    // println!("{:?}", hs);
+    // 1
+    // for i in values.iter() {}
 }
 
 fn main() {
-    println!("{:?}", find_outlier(&[2, 3, 4, 8, 6]));
+    println!("{:?}", find_outlier(&[2, 9, 4, 8, 6, 10, 12]));
 }
