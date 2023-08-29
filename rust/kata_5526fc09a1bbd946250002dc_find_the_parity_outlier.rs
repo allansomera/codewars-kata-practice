d25615872f180c404958d61b2034a2a9b6ceef9c
Allan Somera <nagal.somera@gmail.com>
2023-08-28 23:55:47 -0700

    worked on kata 5526fc09a1bbd946250002dc

--- rust/kata_5526fc09a1bbd946250002dc_find_the_parity_outlier.rs
+++ rust/kata_5526fc09a1bbd946250002dc_find_the_parity_outlier.rs
@@ -10,9 +10,17 @@ fn find_outlier(values: &[i32]) -> i32 {
         }
         m
     });
-    if *hs.get(&0).unwrap() as i32 >= 2 {
+    if *hs.get(&0).unwrap() > *hs.get(&1).unwrap() {
+        println!(
+            "pos even: {:?}",
+            values.into_iter().position(|&x| x % 2 == 1)
+        );
         values[values.iter().position(|&x| x % 2 == 1).unwrap() as usize]
     } else {
+        println!(
+            "pos odd: {:?}",
+            values.into_iter().position(|&x| x % 2 == 1)
+        );
         values[values.iter().position(|&x| x % 2 == 0).unwrap() as usize]
     }
     // println!("{:?}", hs);
@@ -21,5 +29,6 @@ fn find_outlier(values: &[i32]) -> i32 {
 }
 
 fn main() {
-    println!("{:?}", find_outlier(&[2, 9, 4, 8, 6, 10, 12]));
+    // println!("{:?}", find_outlier(&[2, 9, 4, 8, 6, 10, 12]));
+    println!("{:?}", find_outlier(&[2, -6, 8, -10, -3]));
 }
