b8ff23521c7b047ed898a62cbf5e97f40d551c94
Allan Somera <nagal.somera@gmail.com>
2023-09-07 22:17:10 -0700

    worked on kata 54de3257f565801d96001200

--- kata_54de3257f565801d96001200_ease_the_stockbroker.rs
+++
@@ -1,3 +0,0 @@
-fn balance_statement(lst: &str) -> String {
-    // your code
-}
---
+++ rust/kata_54de3257f565801d96001200_ease_the_stockbroker.rs
@@ -0,0 +1,7 @@
+fn balance_statement(lst: &str) -> String {
+    test.to_string()
+}
+
+fn main() {
+    println!("{:?}", balance_statement("GOOG 300 542.0 B"));
+}
