2458499bb881168b9e14000dd8edb8f626219c23
Allan Somera <nagal.somera@gmail.com>
2023-09-08 23:53:08 -0700

    worked on kata 54de3257f565801d96001200

--- rust/kata_54de3257f565801d96001200_ease_the_stockbroker.rs
+++ rust/kata_54de3257f565801d96001200_ease_the_stockbroker.rs
@@ -1,7 +1,29 @@
 fn balance_statement(lst: &str) -> String {
-    test.to_string()
+    let vs: Vec<String>;
+
+    if lst.contains(",") {
+        vs = lst
+            .split(",")
+            .into_iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>();
+        println!("comma: {:?}", vs);
+    } else {
+        vs = lst
+            .split_whitespace()
+            .into_iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>();
+        println!("ws: {:?}", vs);
+    }
+
+    "test".to_string()
 }
 
 fn main() {
     println!("{:?}", balance_statement("GOOG 300 542.0 B"));
+    println!(
+        "{:?}",
+        balance_statement("GOOG 300 542.0 B,GOOG 300 542.0 B")
+    );
 }
