d0adecea38a63cc13ac899334da3c8142e66d04b
Allan Somera <nagal.somera@gmail.com>
2023-09-12 23:57:35 -0700

    worked on kata 54de3257f565801d96001200

--- rust/kata_54de3257f565801d96001200_ease_the_stockbroker.rs
+++ rust/kata_54de3257f565801d96001200_ease_the_stockbroker.rs
@@ -1,8 +1,23 @@
 // use std::any::type_name;
 
 use std::collections::HashMap;
-fn type_of<T>(_: &T) -> &'static str {
-    std::any::type_name::<T>()
+// fn type_of<T>(_: &T) -> &'static str {
+//     std::any::type_name::<T>()
+// }
+
+fn build(vv: &Vec<String>, multi: bool) -> HashMap<String, HashMap<String, i32>> {
+    let mut data: HashMap<String, HashMap<String, i32>> = HashMap::new();
+    // let stock_info: HashMap<String, i32> = HashMap::new();
+
+    if multi {
+        todo!()
+    } else {
+        data.insert(
+            String::from(&vv[0]),
+            HashMap::from([("Buy".to_string(), 1), ("Sell".to_string(), 2)]),
+        );
+    }
+    data
 }
 
 fn balance_statement(lst: &str) -> String {
@@ -25,21 +40,19 @@ fn balance_statement(lst: &str) -> String {
         vs.iter().for_each(|x| println!("{:?}", x));
     }
 
-    println!("first: {}", vs[0]);
-
-    let data: HashMap<String, HashMap<String, i32>> = HashMap::new();
-
-    for x in vs.iter() {
-        println!("type_of: {:?}", type_of(&x))
-    }
-
+    // println!("first: {}", vs[0]);
+    //
+    // for x in vs.iter() {
+    //     println!("type_of: {:?}", type_of(&x))
+    // }
+    println!("{:?}", build(&vs, false));
     "test".to_string()
 }
 
 fn main() {
     println!("{:?}", balance_statement("GOOG 300 542.0 B"));
-    println!(
-        "{:?}",
-        balance_statement("GOOG 300 542.0 B,GOOG 300 542.0 B")
-    );
+    // println!(
+    //     "{:?}",
+    //     balance_statement("GOOG 300 542.0 B,GOOG 300 542.0 B")
+    // );
 }
