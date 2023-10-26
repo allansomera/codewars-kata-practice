076e36a0aaa55ff9dd98af739a1d02c43c36515c
Allan Somera <nagal.somera@gmail.com>
2023-10-25 22:32:40 -0700

    worked on kata 5596f6e9529e9ab6fb000014

--- rust/kata_5596f6e9529e9ab6fb000014_calculate_string_rotation.rs
+++ rust/kata_5596f6e9529e9ab6fb000014_calculate_string_rotation.rs
@@ -1,7 +1,23 @@
 fn shifted_diff(first: &str, second: &str) -> Option<usize> {
-    todo!();
+    let mut split: Vec<String> = first
+        .chars()
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>();
+    // println!("{:?}", split);
+
+    for i in 0..split.len() {
+        split.rotate_right(1);
+        let tmp: String = split.clone().join("");
+        if tmp == second.clone().to_string() {
+            return Some(i);
+        }
+        // println!("split: {:?}", split);
+        // println!("tmp: {:?}", tmp);
+        // println!("second: {:?}", second.clone().to_string())
+    }
+    None
 }
 
 fn main() {
-    println!("{:?}", shifted("eecoff", "coffee"))
+    println!("{:?}", shifted_diff("eecoff", "coffee"));
 }
