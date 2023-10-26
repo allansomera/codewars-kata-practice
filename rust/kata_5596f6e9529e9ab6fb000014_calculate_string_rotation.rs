2ecbc8e87b5a88bc4fd75d4f184846754c7533fe
Allan Somera <nagal.somera@gmail.com>
2023-10-26 12:07:03 -0700

    finished kata 5596f6e9529e9ab6fb000014 calculate string rotation, rust

--- rust/kata_5596f6e9529e9ab6fb000014_calculate_string_rotation.rs
+++ rust/kata_5596f6e9529e9ab6fb000014_calculate_string_rotation.rs
@@ -6,11 +6,11 @@ fn shifted_diff(first: &str, second: &str) -> Option<usize> {
     // println!("{:?}", split);
 
     for i in 0..split.len() {
-        split.rotate_right(1);
         let tmp: String = split.clone().join("");
         if tmp == second.clone().to_string() {
             return Some(i);
         }
+        split.rotate_right(1);
         // println!("split: {:?}", split);
         // println!("tmp: {:?}", tmp);
         // println!("second: {:?}", second.clone().to_string())
@@ -21,3 +21,33 @@ fn shifted_diff(first: &str, second: &str) -> Option<usize> {
 fn main() {
     println!("{:?}", shifted_diff("eecoff", "coffee"));
 }
+
+// soln:
+// fn shifted_diff(first: &str, second: &str) -> Option<usize> {
+//     if first.len() > second.len() { return None }
+//     second.repeat(2).find(first)
+// }
+//
+// fn shifted_diff(first: &str, second: &str) -> Option<usize> {
+//     (0..second.len()).map(|i| second[i..].to_string() + &second[..i]).position(|s| s == first)
+// }
+//
+// fn shifted_diff(s: &str, t: &str) -> Option<usize> {
+//     (0..s.len()).position(|i| s[s.len()-i..].to_string()+&s[0..s.len()-i]==t)
+// }
+//
+// fn shifted_diff(first: &str, second: &str) -> Option<usize> {
+//     if first.len()!=second.len() {return None;}
+//     (String::from(second)+second).find(first)
+// }
+//
+// fn shifted_diff(first: &str, second: &str) -> Option<usize> {
+//     let mut s = second.to_string();
+//     (0..=first.len()).find(|&v|{
+//         if v > 0 {
+//             let c = s.remove(0);
+//             s.push(c);
+//         }
+//         if &s == first { true } else { false }
+//     })
+// }
