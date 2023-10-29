e746b323e1645fd803fb94b0d4f2af493a5a8e4b
Allan Somera <nagal.somera@gmail.com>
2023-10-28 22:49:42 -0700

    worked on kata 554a44516729e4d80b000012

--- rust/kata_554a44516729e4d80b000012_buying_a_car.rs
+++ rust/kata_554a44516729e4d80b000012_buying_a_car.rs
@@ -1,7 +1,12 @@
 fn nb_months(old: i32, new: i32, saving: i32, perc: f64) -> (i32, i32) {
-    todo!();
+    let old_dep = (old as f64 / (1.0 + (perc * 100.0))) as f64;
+    let new_dep = (new as f64 / (1.0 + (perc * 100.0))) as f64;
+
+    println!("old_dep {:?}", old_dep);
+    println!("new_dep {:?}", new_dep);
+    (0, 0)
 }
 
 fn main() {
-    println!("{:?}", nb_months(2000, 8000, 1.5, (6, 766)))
+    println!("{:?}", nb_months(2000, 8000, 1000, 1.5));
 }
