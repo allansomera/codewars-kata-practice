f78dbca96a395299b1d2871ca6515b7c2922aeb4
Allan Somera <nagal.somera@gmail.com>
2023-11-01 23:54:33 -0700

    worked on kata 554a44516729e4d80b000012

--- rust/kata_554a44516729e4d80b000012_buying_a_car.rs
+++ rust/kata_554a44516729e4d80b000012_buying_a_car.rs
@@ -2,9 +2,17 @@ fn nb_months(old: i32, new: i32, saving: i32, perc: f64) -> (i32, i32) {
     let old_dep = (old as f64 / (1.0 + (perc / 100.0))) as f64;
     let new_dep = (new as f64 / (1.0 + (perc / 100.0))) as f64;
     let p = perc.clone();
+    let mut calc = 1_f64;
+    let mut new_base;
 
     // println!("old_dep = {:?} / ({:?} + {:?})", old, 1.0, perc / 100.0);
-    let calc = new_dep - old_dep - saving as f64;
+    while calc > 0_f64 {
+        calc = new_dep - old_dep - saving as f64;
+        new_base = calc.clone();
+        // println!("old_dep {:?}", old_dep);
+        // println!("new_dep {:?}", new_dep);
+        println!("calc: {:?}", calc);
+    }
     println!("old_dep {:?}", old_dep);
     println!("new_dep {:?}", new_dep);
     println!("calc: {:?}", calc);
