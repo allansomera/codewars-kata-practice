54f6bf70608ccff2bbc1bd9bddf659480f8b5280
Allan Somera <nagal.somera@gmail.com>
2023-11-03 22:33:41 -0700

    worked on kata 554a44516729e4d80b000012

--- rust/kata_554a44516729e4d80b000012_buying_a_car.rs
+++ rust/kata_554a44516729e4d80b000012_buying_a_car.rs
@@ -4,19 +4,16 @@ fn nb_months(old: i32, new: i32, saving: i32, perc: f64) -> (i32, i32) {
     let p = perc.clone();
     let mut calc: f64 = 0f64;
     let mut new_base = 0f64;
+    calc = (old_dep - new_dep) - saving as f64;
 
-    while true {
-        if calc < 0f64 {
-            old_dep = (old_dep as f64 / (1.0 + (perc / 100.0))) as f64;
-            new_dep = (new_dep as f64 / (1.0 + (perc / 100.0))) as f64;
-            calc = new_dep - old_dep - saving as f64;
-            new_base = calc.clone();
-        }
-        println!("old_dep {:?}", old_dep);
-        println!("new_dep {:?}", new_dep);
-        println!("calc: {:?}", calc);
-        println!("new_base {:?}", new_base);
-    }
+    old_dep = (old_dep as f64 / (1.0 + (perc / 100.0))) as f64;
+    new_dep = (new_dep as f64 / (1.0 + (perc / 100.0))) as f64;
+    calc = (old_dep - new_dep) - saving as f64;
+    new_base = calc.clone();
+    println!("old_dep {:?}", old_dep);
+    println!("new_dep {:?}", new_dep);
+    println!("calc: {:?}", calc);
+    println!("new_base {:?}", new_base);
 
     (0, 0)
 }
