38f2000a7bfa7291445802513d2bf0d08746eff1
Allan Somera <nagal.somera@gmail.com>
2023-11-04 22:49:43 -0700

    worked on kata 554a44516729e4d80b000012

--- rust/kata_554a44516729e4d80b000012_buying_a_car.rs
+++ rust/kata_554a44516729e4d80b000012_buying_a_car.rs
@@ -1,19 +1,23 @@
 fn nb_months(old: i32, new: i32, saving: i32, perc: f64) -> (i32, i32) {
     let mut old_dep = (old as f64 / (1.0 + (perc / 100.0))) as f64;
     let mut new_dep = (new as f64 / (1.0 + (perc / 100.0))) as f64;
+    println!("old_dep {:?}", old_dep);
+    println!("new_dep {:?}", new_dep);
     let p = perc.clone();
     let mut calc: f64 = 0f64;
     let mut new_base = 0f64;
-    calc = (old_dep - new_dep) - saving as f64;
-
-    old_dep = (old_dep as f64 / (1.0 + (perc / 100.0))) as f64;
-    new_dep = (new_dep as f64 / (1.0 + (perc / 100.0))) as f64;
-    calc = (old_dep - new_dep) - saving as f64;
-    new_base = calc.clone();
-    println!("old_dep {:?}", old_dep);
-    println!("new_dep {:?}", new_dep);
+    let old_saving = old_dep + saving as f64;
+    println!("old_saving {:?}", old_saving);
+    calc = old_saving - new_dep;
     println!("calc: {:?}", calc);
-    println!("new_base {:?}", new_base);
+
+    // old_dep = (old_dep as f64 / (1.0 + (perc / 100.0))) as f64;
+    // new_dep = (new_dep as f64 / (1.0 + (perc / 100.0))) as f64;
+    // calc = (old_dep - new_dep) - saving as f64;
+    // new_base = calc.clone();
+    // println!("old_dep {:?}", old_dep);
+    // println!("new_dep {:?}", new_dep);
+    // println!("new_base {:?}", new_base);
 
     (0, 0)
 }
