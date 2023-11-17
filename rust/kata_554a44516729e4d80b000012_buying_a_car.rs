ed78fff5f4141eb1aea6ba1b9dcc89dc95f00440
Allan Somera <nagal.somera@gmail.com>
2023-11-16 23:16:57 -0800

    worked on kata 554a44516729e4d80b000012

--- rust/kata_554a44516729e4d80b000012_buying_a_car.rs
+++ rust/kata_554a44516729e4d80b000012_buying_a_car.rs
@@ -3,10 +3,11 @@ fn nb_months(old: i32, new: i32, saving: i32, perc: f64) -> (i32, i32) {
     // println!("{:?}", calc);
     // println!("perc {:?}", perc);
     let mut old_f = old as f64 * (1.0 - (perc / 100.0));
-    // println!("old_f {:?}", old_f);
+    println!("old_f {:?}", old_f);
     let mut new_f = new as f64 * (1.0 - (perc / 100.0));
+    println!("new_f {:?}", new_f);
     let mut perc_new = perc.clone();
-    // println!("new_f {:?}", new_f);
+    println!("perc_new: {:?}", perc_new);
     let mut old_saving = old_f + saving as f64;
     let mut saved = 0.0;
     // println!("{:?}", old_saving - new_f);
@@ -20,14 +21,14 @@ fn nb_months(old: i32, new: i32, saving: i32, perc: f64) -> (i32, i32) {
 
     while target {
         old_f = old_f as f64 * (1.0 - (perc_new / 100.0));
-        // println!("old_f: {:?}", old_f);
+        println!("old_f: {:?}", old_f);
         new_f = new_f as f64 * (1.0 - (perc_new / 100.0));
-        // println!("new_f: {:?}", new_f);
+        println!("new_f: {:?}", new_f);
         old_saving = old_f + saving as f64 + saved as f64;
         println!("perc_new: {:?}", perc_new);
         calc = old_saving - new_f;
-        // println!("calc: {:?}", calc);
-        // println!("month: {:?}", month);
+        println!("calc: {:?}", calc);
+        println!("month: {:?}", month);
         month += 1;
         saved += saving as f64;
         if month % 2 == 0 {
