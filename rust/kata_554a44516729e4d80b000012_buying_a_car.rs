bc4d5c88e6b42a853466f822bb0c7a20b58b3405
Allan Somera <nagal.somera@gmail.com>
2023-11-18 21:59:38 -0800

    worked on kata 554a44516729e4d80b000012

--- rust/kata_554a44516729e4d80b000012_buying_a_car.rs
+++ rust/kata_554a44516729e4d80b000012_buying_a_car.rs
@@ -19,26 +19,26 @@ fn nb_months(old: i32, new: i32, saving: i32, perc: f64) -> (i32, i32) {
     // let oo_s = oo + saved + saving as f64 - nf;
     // println!("{:?}", oo_s);
 
-    while target {
-        old_f = old_f as f64 * (1.0 - (perc_new / 100.0));
-        println!("old_f: {:?}", old_f);
-        new_f = new_f as f64 * (1.0 - (perc_new / 100.0));
-        println!("new_f: {:?}", new_f);
-        old_saving = old_f + saving as f64 + saved as f64;
-        println!("old_saving: {:?}", old_saving);
-        println!("perc_new: {:?}", perc_new);
-        calc = old_saving - new_f;
-        println!("calc: {:?}", calc);
-        println!("month: {:?}", month);
-        month += 1;
-        saved += saving as f64;
-        if month % 2 == 0 {
-            perc_new += 0.5;
-        }
-        if calc > 0.0 {
-            target = false;
-        }
-    }
+    // while target {
+    //     old_f = old_f as f64 * (1.0 - (perc_new / 100.0));
+    //     // println!("old_f: {:?}", old_f);
+    //     new_f = new_f as f64 * (1.0 - (perc_new / 100.0));
+    //     println!("new_f: {:?}", new_f);
+    //     old_saving = old_f + saving as f64 + saved as f64;
+    //     println!("old_saving: {:?}", old_saving);
+    //     println!("perc_new: {:?}", perc_new);
+    //     calc = old_saving - new_f;
+    //     println!("calc: {:?}", calc);
+    //     println!("month: {:?}", month);
+    //     month += 1;
+    //     saved += saving as f64;
+    //     if month % 2 == 0 {
+    //         perc_new += 0.5;
+    //     }
+    //     if calc > 0.0 {
+    //         target = false;
+    //     }
+    // }
 
     (0, 0)
 }
