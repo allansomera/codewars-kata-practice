362e0e424fdb32b19d2945e36d0d52b42bc4b86b
Allan Somera <nagal.somera@gmail.com>
2023-11-29 23:03:58 -0800

    worked on kata 578aa45ee9fd15ff4600090d

--- rust/kata_578aa45ee9fd15ff4600090d_sort_the_odd.rs
+++ rust/kata_578aa45ee9fd15ff4600090d_sort_the_odd.rs
@@ -1,3 +1,13 @@
 fn sort_array(arr: &[i32]) -> Vec<i32> {
-    todo!();
+    let mut v: Vec<i32> = arr
+        .iter()
+        .filter(|x| *x % 2 == 0)
+        .copied()
+        .collect::<Vec<i32>>();
+    println!("v: {:?}", v);
+    v
+}
+
+fn main() {
+    println!("{:?}", sort_array(&[5, 3, 2, 8, 1, 4]));
 }
