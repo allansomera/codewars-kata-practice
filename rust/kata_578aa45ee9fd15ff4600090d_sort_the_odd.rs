a78778071a1fab7086fe4d0c81d4658c5a783962
Allan Somera <nagal.somera@gmail.com>
2023-12-02 22:49:25 -0800

    worked on kata 578aa45ee9fd15ff4600090d

--- rust/kata_578aa45ee9fd15ff4600090d_sort_the_odd.rs
+++ rust/kata_578aa45ee9fd15ff4600090d_sort_the_odd.rs
@@ -1,18 +1,27 @@
 fn sort_array(arr: &[i32]) -> Vec<i32> {
-    // let mut v: Vec<i32> = arr
-    //     .into_iter()
-    //     .filter(|x| *x % 2 == 0)
-    //     .copied()
-    //     .collect::<Vec<i32>>();
-    // println!("v: {:?}", v);
-    // v
     let mut v: Vec<i32> = arr
-        .iter()
-        .enumerate()
-        .filter(|(_, &el)| el % 2 == 0)
+        .into_iter()
+        // .filter(|x| *x % 2 == 0)
         .copied()
         .collect::<Vec<i32>>();
+    // v
+    let mut pos: Vec<usize> = Vec::new();
+    let odd: Vec<i32> = arr
+        .into_iter()
+        .filter(|x| *x % 2 != 0)
+        .copied()
+        .collect::<Vec<i32>>();
+    let new_var = vec![0; arr.len()];
+
+    for (i, el) in arr.iter().enumerate() {
+        if el % 2 == 0 {
+            pos.push(i);
+        }
+    }
     println!("v: {:?}", v);
+    println!("pos: {:?}", pos);
+    println!("odd: {:?}", odd);
+
     v
 }
 
