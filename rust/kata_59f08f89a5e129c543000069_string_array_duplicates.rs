780cb0baabca31ac5c82f1a4eabad349a37062c5
Allan Somera <nagal.somera@gmail.com>
2023-12-08 23:42:43 -0800

    worked on kata 59f08f89a5e129c543000069

--- rust/kata_59f08f89a5e129c543000069_string_array_duplicates.rs
+++ rust/kata_59f08f89a5e129c543000069_string_array_duplicates.rs
@@ -2,6 +2,14 @@ use regex::Regex;
 
 fn dup(arry: Vec<String>) -> Vec<String> {
     let re = Regex::new(r"(.)+").unwrap();
+    for (i, el) in arry.iter().enumerate() {
+        let matches = re
+            .find_iter(*el)
+            .filter_map(|str| str.as_str().parse().ok())
+            .collect();
+        println!("matches: {:?}", matches);
+    }
+    todo!()
 }
 
 fn main() {
