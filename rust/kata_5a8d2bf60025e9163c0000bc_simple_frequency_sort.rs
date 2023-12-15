72baf6842f31107cefdb956bf98fbd2b8e4a2a60
Allan Somera <nagal.somera@gmail.com>
2023-12-14 23:42:15 -0800

    worked on kata 5a8d2bf60025e9163c0000bc

--- rust/kata_5a8d2bf60025e9163c0000bc_simple_frequency_sort.rs
+++ rust/kata_5a8d2bf60025e9163c0000bc_simple_frequency_sort.rs
@@ -1,7 +1,18 @@
+use std::collections::HashMap;
 fn solve(vec: &[i32]) -> Vec<i32> {
     let mut c: Vec<i32> = vec.into_iter().map(|x| *x as i32).collect::<Vec<i32>>();
-    c.sort_unstable_by(|a,b|);
+    let mut cnt = HashMap::new();
+    for (i, el) in c.iter().enumerate() {
+        cnt.insert(*el, cnt.get(&el).unwrap_or(&0) + 1);
+    }
+    let mut cs = cnt.into_iter().collect::<Vec<(i32, i32)>>();
+    cs.sort_unstable_by(|(_, a), (_, b)| b.cmp(&a));
+    cs.into_iter()
+        .map(|(c, v)| vec![c; v as usize].into_iter().collect::<i32>())
+        .collect();
+
     println!("c: {:?}", c);
+    println!("cs: {:?}", cs);
     c
 }
 
