c60fa9c49e7f126bf549658c55d1bb204c7c27c6
Allan Somera <nagal.somera@gmail.com>
2023-12-30 23:00:26 -0800

    worked on kata 587c0138110b20624e000253

--- rust/kata_587c0138110b20624e000253_esolang_minibitmove.rs
+++ rust/kata_587c0138110b20624e000253_esolang_minibitmove.rs
@@ -4,6 +4,8 @@ fn interpreter(tape: &str, data: &str) -> String {
     //     "0" =>
     //
     // }).collect::<String>();
+    let mut tape_idx = 0;
+    let mut idx;
     println!("tape: {:?}", tape);
     println!("data: {:?}", data);
     "test".to_string()
